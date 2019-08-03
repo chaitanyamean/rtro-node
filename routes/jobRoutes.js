de4d940d3a92f571ea86f30bd9b4350e08e779dc
chaitanyamean <chaitanyamean@gmail.com>
2019-08-03 11:33:05 +0530

    heroku setup

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -29,7 +29,7 @@ const response = require('../libs/responseLibs')
  */
 var elasticsearch = require('elasticsearch')
 var client = new elasticsearch.Client({
-  host: 'localhost:9200'
+  host: 'https://site:0bc25b65fa425f5fad0303ec737d637b@thorin-us-east-1.searchly.com'
 })
 
 /**
